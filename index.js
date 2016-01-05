e4c1101c61dabcd68bf527462019e1adc121646e
Linda Rian <lindarian12@gmail.com>
2016-01-05 17:32:02 +0100

    add findBank function

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-module.exports = function one() {
-  return 1;
+const banks = [
+  require('./banks/alfabank'),
+  require('./banks/raiffeisen'),
+  require('./banks/sberbank'),
+  require('./banks/tinkoff'),
+  require('./banks/vtb24'),
+];
+
+module.exports = function findBank(cardNumber) {
+  const first5 = parseInt(cardNumber.substr(0, 5), 10);
+  const first6 = parseInt(cardNumber.substr(0, 6), 10);
+
+  var i;
+  var j;
+  var bank;
+  var prefix;
+
+  for (i = 0; i < banks.length; i++) {
+    bank = banks[i];
+
+    for (j = 0; j < bank.prefixes.length; j++) {
+      prefix = bank.prefixes[j];
+
+      if (prefix === first5 || prefix === first6) {
+        return bank;
+      }
+    }
+  }
 };
--- test.js
+++ test.js
@@ -2,5 +2,21 @@ const test = require('ava');
 const banksDB = require('.');
 
 test('returns 1', t => {
-  t.same(banksDB(), 1);
+  t.same(banksDB('5211780000000000'), {
+    name: 'alfabank',
+    prefixes: [
+      521178,
+      548673,
+      548601,
+      45841,
+      415428,
+      676371,
+      477964,
+    ],
+    country: 'ru',
+    localTitle: 'Альфа-Банк',
+    engTitle: 'Alfa-Bank',
+    url: 'https://alfabank.ru/',
+    color: '#F22F17',
+  });
 });
