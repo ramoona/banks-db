63efb38e47e4e0c740cc851a45aaf010cf59df8f
Linda Rian <lindarian12@gmail.com>
2016-01-07 20:23:44 +0100

    add checking of missing requires

--- lint.js
+++ lint.js
@@ -13,16 +13,31 @@ function readJSON(file, callback) {
   });
 }
 
-readJSON('schema.json', schema => {
-  fs.readdir(path.join(__dirname, 'banks'), (err, files) => {
-    if (err) throw err;
+fs.readdir(path.join(__dirname, 'banks'), (err, files) => {
+  if (err) throw err;
+
+  fs.readFile(path.join(__dirname, 'index.js'), (err2, indexData) => {
+    if (err2) throw err2;
+    const index = indexData.toString();
+    files.forEach(name => {
+      var line = "require('./banks/" + name.replace(/\.json$/, '') + "')";
+      if (index.indexOf(line) === -1) {
+        console.error(chalk.red('FAIL index.js'));
+        console.error('Missed ' + line);
+        process.exit(1);
+      }
+    });
+    console.log(chalk.green('OK ') + chalk.white('index.js'));
+  });
+
+  readJSON('schema.json', schema => {
     files.forEach(name => {
       readJSON('banks/' + name, bank => {
         const report = linter.validate(bank, schema);
         if (report.errors.length === 0) {
-          console.log(chalk.green('OK ') + chalk.white(name));
+          console.log(chalk.green('OK ') + chalk.white('banks/' + name));
         } else {
-          console.error(chalk.red('FAIL ' + name));
+          console.error(chalk.red('FAIL ' + 'banks/' + name));
           report.errors.forEach(i => console.error(i));
           process.exit(1);
         }
