6a985379831fab4f95a56770e87ace210b637fa4
Ramoona <lindarian12@gmail.com>
2016-02-16 22:20:22 +0300

    close #33

--- index.js
+++ index.js
@@ -36,15 +36,14 @@ const banks = [
 
 const prefixes = {};
 
-var i;
-var j;
-for (i = 0; i < banks.length; i++) {
-  for (j = 0; j < banks[i].prefixes.length; j++) {
+for (var i = 0; i < banks.length; i++) {
+  for (var j = 0; j < banks[i].prefixes.length; j++) {
     prefixes[banks[i].prefixes[j]] = banks[i];
   }
 }
 
 module.exports = function findBank(cardNumber) {
+  cardNumber = cardNumber || '';
   const card = cardNumber.toString().replace(/[^\d]/g, '');
   const first5 = card.substr(0, 5);
   const first6 = card.substr(0, 6);
@@ -53,9 +52,8 @@ module.exports = function findBank(cardNumber) {
     type: type(card)
   };
 
-  var el;
   if (bank) {
-    for (el in bank) {
+    for (var el in bank) {
       result[el] = bank[el];
     }
   }
