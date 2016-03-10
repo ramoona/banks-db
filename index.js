e69da6322acdf2bbc5815bcaeb85e837e8db082c
Ramoona <lindarian12@gmail.com>
2016-03-11 00:54:40 +0300

    fixes for browsers support

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 'use strict';
 
-const type = require('./type');
-const data = require('./banks/index');
-let banks = [];
+var type = require('./type');
+var data = require('./banks/index');
+var banks = [];
 
-data.forEach(item => {
+data.forEach(function (item) {
   banks = banks.concat(item);
 });
 
-banks.forEach(bank => {
+banks.forEach(function (bank) {
   bank.code = bank.country + '-' + bank.name;
 });
 
-const prefixes = {};
+var prefixes = {};
 
-for (let i = 0; i < banks.length; i++) {
-  for (let j = 0; j < banks[i].prefixes.length; j++) {
+for (var i = 0; i < banks.length; i++) {
+  for (var j = 0; j < banks[i].prefixes.length; j++) {
     prefixes[banks[i].prefixes[j]] = banks[i];
   }
 }
 
 module.exports = function findBank(cardNumber) {
   cardNumber = cardNumber || '';
-  const card = cardNumber.toString().replace(/[^\d]/g, '');
-  const first5 = card.substr(0, 5);
-  const first6 = card.substr(0, 6);
-  const bank = prefixes[first6] || prefixes[first5];
-  const result = {
+  var card = cardNumber.toString().replace(/[^\d]/g, '');
+  var first5 = card.substr(0, 5);
+  var first6 = card.substr(0, 6);
+  var bank = prefixes[first6] || prefixes[first5];
+  var result = {
     type: type(card)
   };
 
   if (bank) {
-    for (const el in bank) {
+    for (var el in bank) {
       result[el] = bank[el];
     }
   }
