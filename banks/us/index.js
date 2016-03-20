9fe8635a6535edcf5e7a4ff97df5d2734aefbf1f
malazaridi <maryana.lazaridi@gmail.com>
2016-03-20 15:56:51 -0400

    Added US banks  - Wells Fargo,  - Citi bank (US),  - Capital One bank,  - American Express (might want to split by card type,    since different cards have different colors,    i.e. Amex Green, Amex Gold and etc.),  - HSBC (US),  - Barclays (US)

--- banks/us/index.js
+++ banks/us/index.js
@@ -2,4 +2,10 @@ module.exports = [
   require('./chase'),
   require('./bankofamerica'),
   require('./tdbank'),
+  require('./hsbc'),
+  require('./citibank'),
+  require('./capitalonebank'),
+  require('./americanexpress'),
+  require('./barclays'),
+  require('./wellsfargo')
 ];
