639df1d6abd53ba58a9f971a546062da5a461005
Amio <amio.cn@gmail.com>
2016-04-11 22:01:42 +0800

    Add chinese banks.

---
+++ banks/cn/index.js
@@ -0,0 +1,16 @@
+module.exports = [
+  require('./abc'),
+  require('./boc'),
+  require('./boco'),
+  require('./ccb'),
+  require('./ceb'),
+  require('./cgb'),
+  require('./cib'),
+  require('./citic'),
+  require('./cmb'),
+  require('./cmib'),
+  require('./hxb'),
+  require('./icbc'),
+  require('./pab'),
+  require('./spdb')
+];
