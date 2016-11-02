88390b329022412daf0b7044bcd9dcc8f852df1f
javid <javid@tarameshgroup.com>
2016-11-02 13:23:27 +0330

    Added Karafarin Bank

--- banks/ir/index.js
+++ banks/ir/index.js
@@ -2,6 +2,7 @@ module.exports = [
   require('./eghtesadnovin'),
   require('./exportdevelopment'),
   require('./industryandmine'),
+  require('./karafarin'),
   require('./keshavarzi'),
   require('./maskan'),
   require('./mellat'),
