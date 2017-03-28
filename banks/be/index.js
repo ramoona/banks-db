7c98453eb9a8676d977de076d73000856074b833
Vlad Feninets <v.feninets@gmail.com>
2017-03-28 12:45:21 +0300

    add missing banks from #131, #127, #120 issues

--- banks/be/index.js
+++ banks/be/index.js
@@ -1,4 +1,5 @@
 module.exports = [
   require('./amexbelgiumcorporate'),
+  require('./belfius'),
   require('./ingbelgium')
 ];
--- banks/de/index.js
+++ banks/de/index.js
@@ -1,3 +1,4 @@
 module.exports = [
+  require('./n26'),
   require('./postbank')
 ];
