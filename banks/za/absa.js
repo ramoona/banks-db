9896021bb6e634b3c810b22719be46486f560931
achedeuzot <klemen@sever.fr>
2018-08-14 17:07:54 +0200

    Fixes #157 - Adds Absa card

---
+++ banks/za/absa.js
@@ -0,0 +1,11 @@
+{
+  "name": "absa",
+  "country": "za",
+  "localTitle": "Absa",
+  "engTitle": "Absa",
+  "url": "https://www.absa.co.za/",
+  "color": "#BE0028",
+  "prefixes": [
+    403822
+  ]
+}
