c11cbf84fcf5254558266dd36c6129a871652865
achedeuzot <klemen@sever.fr>
2018-08-14 17:08:43 +0200

    Fixes #155 - Adds Capitec Bank

---
+++ banks/za/capitec.js
@@ -0,0 +1,11 @@
+{
+  "name": "capitec",
+  "country": "za",
+  "localTitle": "Capitec",
+  "engTitle": "Capitec",
+  "url": "https://www.capitecbank.co.za//",
+  "color": "#DAA520",
+  "prefixes": [
+    528497
+  ]
+}
