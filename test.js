1f339e405f2ec466eb6ef1636301da5dcfa20111
ramoona <lindarian12@gmail.com>
2018-10-18 20:51:48 +0300

    Updating deps & migrating tests to jest

--- test.js
+++ test.js
@@ -1,59 +1,34 @@
-const test = require('ava');
 const banksDB = require('.');
 const type = require('./type');
 
-test('finds bank by first 6 symbols', (t) => {
-  t.deepEqual(banksDB('5211784563802833').name, 'alfabank');
-});
+/* eslint-disable no-undef */
 
-test('returns card type', (t) => {
-  t.deepEqual(banksDB('5211784563802833').type, 'mastercard');
-});
+test('returns card type', () => expect(banksDB('5211784563802833').type).toBe('mastercard'));
 
-test('finds bank by first 5 symbols', (t) => {
-  t.deepEqual(banksDB('4622384563802833').name, 'vtb24');
-});
+test('finds bank by first 6 symbols', () => expect(banksDB('5211784563802833').name).toBe('alfabank'));
 
-test('returns false on unknown bank', (t) => {
-  t.deepEqual(typeof banksDB('4111111111111111').name, 'undefined');
-});
+test('finds bank by first 5 symbols', () => expect(banksDB('4622384563802833').name).toBe('vtb24'));
 
-test('returns card type on unknown bank', (t) => {
-  t.deepEqual(banksDB('4111111111111111').type, 'visa');
-});
+test('returns false on unknown bank', () => expect(banksDB('4111111111111111').name).toBeUndefined());
 
-test('finds bank by converted to string card number', (t) => {
-  t.deepEqual(banksDB(4377734563802833).name, 'tinkoff');
-});
+test('returns card type on unknown bank', () => expect(banksDB('4111111111111111').type).toBe('visa'));
 
-test('ignores non-digits symbols in card number', (t) => {
-  t.deepEqual(banksDB('43-77-73-45-63-80-28-33').name, 'tinkoff');
-});
+test('finds bank by converted to string card number', () => expect(banksDB(4377734563802833).name).toBe('tinkoff'));
 
-test('ignores whitespaces', (t) => {
-  t.deepEqual(banksDB('4627 3045 6380 2833').name, 'raiffeisen');
-});
+test('ignores non-digits symbols in card number', () => expect(banksDB('43-77-73-45-63-80-28-33').name).toBe('tinkoff'));
 
-test('accepts undefined', (t) => {
-  t.deepEqual(typeof banksDB(undefined).name, 'undefined');
-});
+test('ignores whitespaces', () => expect(banksDB('4627 3045 6380 2833').name).toBe('raiffeisen'));
 
-test('returns card type', (t) => {
-  t.deepEqual(type(4111111111111111), 'visa');
-});
+test('accepts undefined', () => expect(banksDB(undefined).name).toBeUndefined());
 
-test('returns undefined on unknown card type', (t) => {
-  t.deepEqual(typeof type(123456), 'undefined');
-});
+test('returns card type', () => expect(type(4111111111111111)).toBe('visa'));
 
-test('returns all banks data', (t) => {
-  t.truthy(Array.isArray(banksDB.data));
-});
+test('returns undefined on unknown card type', () => expect(type(123456)).toBeUndefined());
 
-test('returns country + bankname', (t) => {
-  t.deepEqual(banksDB('5211784563802833').code, 'ru-alfabank');
-});
+test('returns all banks data', () => expect(Array.isArray(banksDB.data)).toBeTruthy());
 
-test('returns code from banksDB.data', (t) => {
-  t.deepEqual(typeof banksDB.data[0].code, 'string');
-});
+test('returns country + bankname', () => expect(banksDB('5211784563802833').code).toBe('ru-alfabank'));
+
+test('returns code from banksDB.data', () => expect(typeof banksDB.data[0].code).toBe('string'));
+
+/* eslint-enable no-undef */
