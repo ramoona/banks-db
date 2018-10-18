b4ea49553df65230739c9e6d9e4695cb01eb310b
ramoona <lindarian12@gmail.com>
2018-10-18 21:00:34 +0300

    Close #167

--- banks/ru/index.js
+++ banks/ru/index.js
@@ -2,6 +2,7 @@ module.exports = [
   require('./akbarsbank'),
   require('./alfabank'),
   require('./avangard'),
+  require('./binbank'),
   require('./citibank'),
   require('./gazprombank'),
   require('./mdm'),
